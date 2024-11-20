ecddc6c1f7529a3b1dc08cb1d3794fcb233c3452
emf-99 <113733097+emf-99@users.noreply.github.com>
2024-11-20 11:35:33 -0500

    ✨ initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Get the Start Game button
+const startGameBtn = document.getElementById("start-game-btn");
+
+// Add event listener to redirect to game.html when clicked
+startGameBtn.addEventListener("click", () => {
+  window.location.href = "game.html";
+});
